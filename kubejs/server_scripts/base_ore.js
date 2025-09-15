f94f023cdede379bf1837fe80d9a69548357fe22
L-Bromine <lishaofan_31415926@163.com>
2025-09-15 23:51:15 +0800

    Add anvilcraft block_crush recipe for poppy melon
    Introduces a new anvilcraft block_crush recipe converting 'kubejs:poppy_melon_sand' to 'kubejs:poppy_melon'. Also improves formatting of the recipe event block.

--- kubejs/server_scripts/base_ore.js
+++ kubejs/server_scripts/base_ore.js
@@ -1,4 +1,5 @@
 ServerEvents.recipes(event => {
     event.smelting('minecraft:iron_nugget', 'minecraft:poppy').xp(0.1).cookingTime(200);
-}
-);
+
+    event.recipes.anvilcraft.block_crush('kubejs:poppy_melon','kubejs:poppy_melon_sand');
+});
