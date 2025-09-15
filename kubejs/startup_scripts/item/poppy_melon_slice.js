e7dec8cc818894cdd4eeceab3f996e1e999e9e80
L-Bromine <lishaofan_31415926@163.com>
2025-09-16 00:30:58 +0800

    Update poppy melon items with nutrition and saturation
    Replaced deprecated 'hunger' property with 'nutrition' and adjusted 'saturation' values for poppy_melon_juice and poppy_melon_slice. Also fixed item registration name for poppy_melon_slice.

--- kubejs/startup_scripts/item/poppy_melon_slice.js
+++ kubejs/startup_scripts/item/poppy_melon_slice.js
@@ -1,9 +1,9 @@
 // 使用物品事件注册新物品
 StartupEvents.registry('item', event => {
-    event.create('poppy_melon_juice')
+    event.create('poppy_melon_slice')
         .food(food => {
             food
-                .hunger(3)
-                .saturation(3)
+                .nutrition(3)
+                .saturation(1)
         })
 })
