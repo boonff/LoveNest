8e7c75930cea1d571aa7db86547e0922e8ce78b6
L-Bromine <lishaofan_31415926@163.com>
2025-09-15 18:14:08 +0800

    Update gem_spring.js

--- kubejs/startup_scripts/block/gem_spring.js
+++ kubejs/startup_scripts/block/gem_spring.js
@@ -27,7 +27,7 @@ StartupEvents.registry('block', event => {
 
                     // 在上方位置放置 anvilcraft:melt_gem 方块
                     level.getBlock(abovePos).set('anvilcraft:melt_gem');
-                    // 你也可以在这里添加一些游戏内提示音效或粒子效果
+                    // TODO 添加一些游戏内提示音效或粒子效果
                     // level.playSound(null, abovePos.getX(), abovePos.getY(), abovePos.getZ(), 'block.anvil.place', 1.0, 1.0);
                     // level.addParticle('cloud', abovePos.getX() + 0.5, abovePos.getY() + 0.5, abovePos.getZ() + 0.5, 0, 0, 0);
                 }
