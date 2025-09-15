fe0ada63c9fb4c1a90d81d81dbab764888dbc811
L-Bromine <lishaofan_31415926@163.com>
2025-09-16 00:20:04 +0800

    Add item_compress recipes for poppy melon juice
    Introduces new anvilcraft item_compress recipes to produce poppy melon juice using poppy melon sand and varying amounts of glass bottles.

--- kubejs/server_scripts/iron_line.js
+++ kubejs/server_scripts/iron_line.js
@@ -2,4 +2,9 @@ ServerEvents.recipes(event => {
     event.smelting('minecraft:iron_nugget', 'minecraft:poppy').xp(0.1).cookingTime(200);
 
     event.recipes.anvilcraft.block_crush('kubejs:poppy_melon','kubejs:poppy_melon_sand');
+    
+    event.recipes.anvilcraft.item_compress(['kubejs:poppy_melon_sand', 'minecraft:glass_bottle'], 'kebujs:poppy_melon_juice');
+    event.recipes.anvilcraft.item_compress(['kubejs:poppy_melon_sand', '2x minecraft:glass_bottle'], '2x kebujs:poppy_melon_juice');
+    event.recipes.anvilcraft.item_compress(['kubejs:poppy_melon_sand', '3x minecraft:glass_bottle'], '3x kebujs:poppy_melon_juice');
+
 });
