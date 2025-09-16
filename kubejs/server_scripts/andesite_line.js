7ebf7de5520c54bc6aca8af456686f1ba40e1432
L-Bromine <lishaofan_31415926@163.com>
2025-09-17 00:40:33 +0800

    Add new anvilcraft item_compress recipe
    Introduces an anvilcraft item_compress recipe combining andesite_sugar_block and poppy_melon_juice to yield andesite_alloy and glass bottles. This expands the crafting options for these custom items.

--- kubejs/server_scripts/andesite_line.js
+++ kubejs/server_scripts/andesite_line.js
@@ -1,4 +1,9 @@
 ServerEvents.recipes(event => {
     event.recipes.anvilcraft.block_compress(['minecraft:andesite', 'anvilcraft:sugar_block'], 'kubejs:andesite_sugar_block')
-    event.recipes.anvilcraft.item_inject('minecraft:andesite', 'anvilcraft:sugar_block',  'kubejs:andesite_sugar_block')
+    event.recipes.anvilcraft.item_inject('minecraft:andesite', 'anvilcraft:sugar_block', 'kubejs:andesite_sugar_block')
+
+    event.recipes.anvilcraft.item_compress(
+        ['kubejs:andesite_sugar_block', 'kubejs:poppy_melon_juice', 'kubejs:andesite_sugar_block', 'kubejs:poppy_melon_juice'],
+        [ChanceItemStack.of('create:andesite_alloy'), ChanceItemStack.of('2x minecraft:glass_bottle')]
+    );
 })
