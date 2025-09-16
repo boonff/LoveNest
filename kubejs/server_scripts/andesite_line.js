67eb7085f086c1fdaca145ac4e68553d5465b841
L-Bromine <lishaofan_31415926@163.com>
2025-09-17 00:17:15 +0800

    Add anvilcraft recipes for andesite sugar block
    Introduces block_compress and item_inject recipes using andesite and sugar block to create andesite sugar block in anvilcraft.

--- kubejs/server_scripts/andesite_line.js
+++ kubejs/server_scripts/andesite_line.js
@@ -1,3 +1,4 @@
 ServerEvents.recipes(event => {
-    
+    event.recipes.anvilcraft.block_compress(['minecraft:andesite', 'anvilcraft:sugar_block'], 'kubejs:andesite_sugar_block')
+    event.recipes.anvilcraft.item_inject('minecraft:andesite', 'anvilcraft:sugar_block',  'kubejs:andesite_sugar_block')
 })
