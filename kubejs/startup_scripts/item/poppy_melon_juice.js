4f5fee171d08e8975fe45c3a83d3bfec40317747
boon <qxxpdada@outlook.com>
2025-09-16 15:26:27 +0800

    fix:更新罂粟西瓜汁和罂粟西瓜片的注册信息，简化代码并添加食用动画

--- kubejs/startup_scripts/item/poppy_melon_juice.js
+++ kubejs/startup_scripts/item/poppy_melon_juice.js
@@ -1,12 +1,13 @@
-// 使用物品事件注册新物品
+// 罂粟西瓜汁
 StartupEvents.registry('item', event => {
     event.create('poppy_melon_juice')
         .food(food => {
-            food
-                .nutrition(6)
+            food.nutrition(6)
                 .saturation(0.5)
                 .eaten(ctx => {
                     ctx.player.give('minecraft:glass_bottle')
                 })
         })
+        .useDuration((stack, entity) => 40) // 设置食用时间，单位 ticks
+        .useAnimation('drink')
 })
