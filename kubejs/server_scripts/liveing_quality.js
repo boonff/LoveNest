7817e81d90b3668dbc53a8577fbb45a072778f53
L-Bromine <lishaofan_31415926@163.com>
2025-09-18 20:29:16 +0800

    feat:添加精准采集

--- kubejs/server_scripts/liveing_quality.js
+++ kubejs/server_scripts/liveing_quality.js
@@ -27,14 +27,14 @@ LootJS.modifiers((event) => {
     // 添加虞美人西瓜掉落
     event.addBlockModifier('foand:poppy_melon')
         .addAlternativesLoot(
-            LootEntry.of('minecraft:grass_block'
+            LootEntry.of('foand:poppy_melon'
             ).when(c =>
                 c.matchMainHand(ItemFilter.hasEnchantment("minecraft:silk_touch"))
             ),
             LootEntry.of("foand:poppy_melon_slice", { type: 'minecraft:uniform', min: 3, max: 7 })
         )
 
-    // 其他需设置的精准caiji
+    // 其他需设置的精准采集
     let block_list = []
     block_list.forEach(block => {
 
