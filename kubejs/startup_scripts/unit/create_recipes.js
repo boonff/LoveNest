62cf36d2e261a775099c737a8653308123553628
L-Bromine <lishaofan_31415926@163.com>
2025-09-18 18:53:08 +0800

    完善了一下create模板，但是没有解决权重问题

--- kubejs/startup_scripts/unit/create_recipes.js
+++ kubejs/startup_scripts/unit/create_recipes.js
@@ -1,3 +1,38 @@
+function getIngredient(input) {
+    let result;
+    if (typeof input === "string" ||
+        ('count' in input && 'id' in input)
+    ) {
+        result = Ingredient.of(input);
+    } else if (Array.isArray(input) && _item != [])
+        result = getIngredient(input[0])
+    else
+        result = input;
+    return result;
+}
+
+function getItem(input) {
+    let result;
+    if (typeof input === "string") {
+        result = Item.of(input);
+    } else if (Array.isArray(input) && _item != [])
+        result = getItem(input[0])
+    else
+        result = input;
+    return result;
+}
+
+function getFluid(input) {
+    let result;
+    if (typeof input === "string") {
+        result = Fluid.of(input, 1000);
+    } else if (Array.isArray(input) && input != []) {
+        result = getFluid(input[0])
+    } else
+        result = input;
+    return result;
+}
+
 let createRecipes = {
     event: null,
     init: function (e) {
@@ -5,37 +40,33 @@ let createRecipes = {
         e.recipes.create = this;
     },
     filling: function (_result, _input) {
-        let fluid = Fluid.of(_input[0]);
-        let input = Item.of(_input[1]);
-        let results;
-        if (Array.isArray(_result)) {
-            results = _result.map(result => Item.of(result));
-        } else {
-            results = [Item.of(_result)];
-        }
+        let fluid = getFluid(_input[0]);
+        let input = getIngredient(_input[1]);
+        let result = getItem(_result);
 
         this.event.custom({
             type: 'create:filling',
             ingredients: [
-                { item: input.id, count: input.count },
-                { type: "fluid_stack", fluid: fluid.id, amount: fluid.amount }
+                input,
+                { type: 'fluid_stack', fluid: fluid.id, amount: fluid.amount }
             ],
-            results: results.map(result => { return { id: result.id, count: result.count } })
+            results: [result]
         })
+
     },
     emptying: function (_result, _input) {
-        let fluid = Fluid.of(_result[0]);
-        let input = Item.of(_input);
-        let result = Item.of(_result[1]);
+        let fluid = getFluid(_result[0]);
+        let input = getIngredient(_input);
+        let result = getItem(_result[1]);
 
-        this.event.custom({
+        return this.event.custom({
             type: "create:emptying",
             ingredients: [
-                { item: input.id, count: input.count }
+                input
             ],
             results: [
-                { id: result.id, count: result.count },
-                { amount: fluid.amount, id: fluid.id }
+                result,
+                fluid
             ]
         })
     }
