5b670d4ae27a89170aac1b8421fa21bfc8fda4e9
boon <qxxpdada@outlook.com>
2025-09-19 16:43:27 +0800

    feat: 添加剪刀与罂粟交互功能

--- kubejs/server_scripts/iron_line.js
+++ kubejs/server_scripts/iron_line.js
@@ -30,7 +30,7 @@ BlockEvents.rightClicked((event) => {
     const { level, item, block, player } = event;
 
     if (block.id != 'foand:poppy_melon') return; // 确保右键点击的是目标方块
-    if (!item.is('minecraft:shears')) return; // 检查手持物品是否为剪刀
+    if (!item.is('minecraft:shears')) return; // TODO 将剪刀改为小刀
 
 
     const itemEntity = level.createEntity('item');
@@ -44,5 +44,47 @@ BlockEvents.rightClicked((event) => {
     level.runCommandSilent("playsound minecraft:entity.sheep.shear");// 播放剪刀使用音效
     player.swing();// 播放玩家挥动手臂的动画
     item.damageValue += 1// 消耗剪刀耐久
+
+});
+
+//副手拿剪刀主手拿罂粟，将西瓜苗变为罂粟西瓜苗
+BlockEvents.rightClicked((event) => {
+    const { level, item, block, player } = event;
+    // 确保右键点击的是目标方块
+    if (block.id != 'minecraft:melon_stem'
+        && block.id != 'minecraft:melon_attached_stem'
+    ) return;
+    if (!item.is('minecraft:shears')) return; // TODO 将剪刀改为小刀
+
+    const mainhand = player.offHandItem;
+    if (!mainhand.is('minecraft:poppy')) return; // 确保主手拿着罂粟
+
+    level.getBlock(block.pos).set('foand:poppy_melon_stem');
+
+    level.runCommandSilent("playsound minecraft:entity.sheep.shear");// 播放剪刀使用音效
+    player.swing();// 播放玩家挥动手臂的动画
+    item.damageValue += 1// 消耗剪刀耐久
+    if (!player.isCreative()) {
+        mainhand.count -= 1;// 消耗主手罂粟
+    }
+
+});
+
+//拿剪刀将罂粟西瓜苗变为西瓜苗
+BlockEvents.rightClicked((event) => {
+    const { level, item, block, player } = event;
+    // 确保右键点击的是目标方块
+    if (block.id != 'foand:poppy_melon_stem'
+        && block.id != 'foand:poppy_melon_attached_stem'
+    ) return;
+    if (!item.is('minecraft:shears')) return; // TODO 将剪刀改为小刀
+
+    level.getBlock(block.pos).set('minecraft:melon_stem');//TODO 西瓜苗age无法设置
+    level.runCommandSilent("playsound minecraft:entity.sheep.shear");// 播放剪刀使用音效
+    player.swing();// 播放玩家挥动手臂的动画
+    item.damageValue += 1// 消耗剪刀耐久
+    if (!player.isCreative()) {
+        player.giveItem(Item.of('minecraft:poppy'));// 给予玩家罂粟
+    }
 
 });
