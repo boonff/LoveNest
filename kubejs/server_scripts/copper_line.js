7398b6d8816534b0020495f16047e2dca56661c4
L-Bromine <lishaofan_31415926@163.com>
2025-09-19 17:27:16 +0800

    添加搅拌蟹笼产生蟹钳方法

--- kubejs/server_scripts/copper_line.js
+++ kubejs/server_scripts/copper_line.js
@@ -1,6 +1,11 @@
 ServerEvents.recipes(event => {
     global.createRecipes.init(event)
 
-    event.recipes.create.mixing(['anvilcraft:crab_trap', 'anvilcraft:crab_claw'], [Fluid.water(1000), 'anvilcraft:crab_trap'])
+    event.recipes.create.mixing([
+        'anvilcraft:crab_trap',
+        { id: 'anvilcraft:crab_claw', chance: 0.2 },
+        { id: 'minecraft:seagrass', chance: 0.3 },
+        { id: 'minecraft:kelp', chance: 0.1 }
+    ], [Fluid.water(1000), 'anvilcraft:crab_trap'])
 
 })
