240aecc5fd7606e7ce0fa6b8a0d78731475b3d90
L-Bromine <lishaofan_31415926@163.com>
2025-09-20 17:41:48 +0800

    添加机械手分配法

--- kubejs/server_scripts/andesite_line.js
+++ kubejs/server_scripts/andesite_line.js
@@ -1,6 +1,9 @@
 ServerEvents.recipes(event => {
+    global.createRecipes.init(event)
+
     event.recipes.anvilcraft.block_compress(['minecraft:andesite', 'anvilcraft:sugar_block'], 'foand:andesite_sugar_block')
     event.recipes.anvilcraft.item_inject('minecraft:andesite', 'anvilcraft:sugar_block', 'foand:andesite_sugar_block')
+    event.recipes.create.deploying('foand:andesite_sugar_block', ['anvilcraft:sugar_block', 'minecraft:andesite'])
 
     event.recipes.anvilcraft.item_compress(
         ['foand:andesite_sugar_block', 'foand:poppy_melon_juice', 'foand:andesite_sugar_block', 'foand:poppy_melon_juice'],
