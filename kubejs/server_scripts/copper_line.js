5eec78beb12f3990d5c7a5a19b8d091c44d762b9
L-Bromine <lishaofan_31415926@163.com>
2025-09-20 16:53:57 +0800

    Update copper_line.js

--- kubejs/server_scripts/copper_line.js
+++ kubejs/server_scripts/copper_line.js
@@ -1,5 +1,5 @@
 ServerEvents.recipes(event => {
-    createRecipes.init(event)
+    global.createRecipes.init(event)
 
     event.recipes.create.mixing([
         'anvilcraft:crab_trap',
@@ -17,11 +17,12 @@ ServerEvents.recipes(event => {
             fluid: 'create:potion',
             amount: 1000,
             components: {
+                'create:potion_fluid_bottle_type': "regular",
                 'minecraft:potion_contents': { potion: 'minecraft:awkward' }
             }
         },
         'anvilcraft:crab_trap'
     ])
 
-    event.recipes.create.mixing('minecraft:enchanted_golden_apple', 'minecraft:shulker_box[container=[{item:{count:64,id:"minecraft:gold_block"},slot:0},{item:{count:64,id:"minecraft:gold_block"},slot:1},{item:{count:64,id:"minecraft:gold_block"},slot:2},{item:{count:64,id:"minecraft:gold_block"},slot:3},{item:{count:64,id:"minecraft:gold_block"},slot:4},{item:{count:64,id:"minecraft:gold_block"},slot:5},{item:{count:64,id:"minecraft:gold_block"},slot:6},{item:{count:64,id:"minecraft:gold_block"},slot:7},{item:{count:64,id:"minecraft:gold_block"},slot:8},{item:{count:64,id:"minecraft:gold_block"},slot:9},{item:{count:64,id:"minecraft:gold_block"},slot:10},{item:{count:64,id:"minecraft:gold_block"},slot:11},{item:{count:64,id:"minecraft:gold_block"},slot:12},{item:{count:1,id:"minecraft:apple"},slot:13},{item:{count:64,id:"minecraft:gold_block"},slot:14},{item:{count:64,id:"minecraft:gold_block"},slot:15},{item:{count:64,id:"minecraft:gold_block"},slot:16},{item:{count:64,id:"minecraft:gold_block"},slot:17},{item:{count:64,id:"minecraft:gold_block"},slot:18},{item:{count:64,id:"minecraft:gold_block"},slot:19},{item:{count:64,id:"minecraft:gold_block"},slot:20},{item:{count:64,id:"minecraft:gold_block"},slot:21},{item:{count:64,id:"minecraft:gold_block"},slot:22},{item:{count:64,id:"minecraft:gold_block"},slot:23},{item:{count:64,id:"minecraft:gold_block"},slot:24},{item:{count:64,id:"minecraft:gold_block"},slot:25},{item:{count:64,id:"minecraft:gold_block"},slot:26}]]')
+    event.recipes.create.compacting('minecraft:enchanted_golden_apple', 'minecraft:shulker_box[container=[{item:{count:64,id:"minecraft:gold_block"},slot:0},{item:{count:64,id:"minecraft:gold_block"},slot:1},{item:{count:64,id:"minecraft:gold_block"},slot:2},{item:{count:64,id:"minecraft:gold_block"},slot:3},{item:{count:64,id:"minecraft:gold_block"},slot:4},{item:{count:64,id:"minecraft:gold_block"},slot:5},{item:{count:64,id:"minecraft:gold_block"},slot:6},{item:{count:64,id:"minecraft:gold_block"},slot:7},{item:{count:64,id:"minecraft:gold_block"},slot:8},{item:{count:64,id:"minecraft:gold_block"},slot:9},{item:{count:64,id:"minecraft:gold_block"},slot:10},{item:{count:64,id:"minecraft:gold_block"},slot:11},{item:{count:64,id:"minecraft:gold_block"},slot:12},{item:{count:1,id:"minecraft:apple"},slot:13},{item:{count:64,id:"minecraft:gold_block"},slot:14},{item:{count:64,id:"minecraft:gold_block"},slot:15},{item:{count:64,id:"minecraft:gold_block"},slot:16},{item:{count:64,id:"minecraft:gold_block"},slot:17},{item:{count:64,id:"minecraft:gold_block"},slot:18},{item:{count:64,id:"minecraft:gold_block"},slot:19},{item:{count:64,id:"minecraft:gold_block"},slot:20},{item:{count:64,id:"minecraft:gold_block"},slot:21},{item:{count:64,id:"minecraft:gold_block"},slot:22},{item:{count:64,id:"minecraft:gold_block"},slot:23},{item:{count:64,id:"minecraft:gold_block"},slot:24},{item:{count:64,id:"minecraft:gold_block"},slot:25},{item:{count:64,id:"minecraft:gold_block"},slot:26}]]')
 })
