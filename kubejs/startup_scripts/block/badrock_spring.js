6b138f7756eb973764a007748cf856dc3d410ed1
boon <qxxpdada@outlook.com>
2025-09-22 15:53:47 +0800

    bugfis:修复岩浆涌泉在水下生成熔岩的bug

--- kubejs/startup_scripts/block/badrock_spring.js
+++ kubejs/startup_scripts/block/badrock_spring.js
@@ -35,15 +35,17 @@ StartupEvents.registry('block', event => {
             let level = event.level;
             let randomChance = Math.random();
             if (randomChance < 0.1) {
-                const abovePos = pos.offset(0, 1, 0);
-                if (level.getBlock(abovePos).id == 'minecraft:air') {
-                    level.getBlock(abovePos).set('minecraft:lava');
+                const topPos = pos.offset(0, 1, 0);
+                const top2Pos = pos.offset(0, 2, 0);
+                if (level.getBlock(topPos).id == 'minecraft:air'
+                    && level.getBlock(top2Pos).id == 'minecraft:air') {
+                    level.getBlock(topPos).set('minecraft:lava');
                     // TODO 添加一些游戏内提示音效或粒子效果
                     // level.playSound(null, abovePos.getX(), abovePos.getY(), abovePos.getZ(), 'block.anvil.place', 1.0, 1.0);
                     // level.addParticle('cloud', abovePos.getX() + 0.5, abovePos.getY() + 0.5, abovePos.getZ() + 0.5, 0, 0, 0);
                 }
-                else if (level.getBlock(abovePos).id == 'minecraft:water') {
-                    level.getBlock(abovePos).set('minecraft:magma_block');
+                else if (level.getBlock(topPos).id == 'minecraft:water') {
+                    level.getBlock(topPos).set('minecraft:magma_block');
                 }
             }
         })
