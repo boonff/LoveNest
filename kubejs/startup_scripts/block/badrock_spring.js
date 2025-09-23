73aced85166e3d3e408ca42a07d8fee69eaf15a4
L-Bromine <lishaofan_31415926@163.com>
2025-09-23 18:18:25 +0800

    fix：修改宝石涌泉生成熔融宝石块

--- kubejs/startup_scripts/block/badrock_spring.js
+++ kubejs/startup_scripts/block/badrock_spring.js
@@ -18,7 +18,7 @@ StartupEvents.registry('block', event => {
                 const abovePos = pos.offset(0, 1, 0);// 获取当前方块正上方的位置
                 // 检查上方位置的方块是否是“可替换”的
                 if (level.getBlock(abovePos) == 'minecraft:air') {
-                    level.getBlock(abovePos).set('anvilcraft:melt_gem');// 在上方位置放置 anvilcraft:melt_gem 方块
+                    level.getBlock(abovePos).set('foand:melt_gem_block');// 在上方位置放置 anvilcraft:melt_gem 方块
                     // TODO 添加一些游戏内提示音效或粒子效果
                     // level.playSound(null, abovePos.getX(), abovePos.getY(), abovePos.getZ(), 'block.anvil.place', 1.0, 1.0);
                     // level.addParticle('cloud', abovePos.getX() + 0.5, abovePos.getY() + 0.5, abovePos.getZ() + 0.5, 0, 0, 0);
