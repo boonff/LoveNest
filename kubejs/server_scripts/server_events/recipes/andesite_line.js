f3016f8e950be488f8584d5a972d3ddb585cd4b5
boon <qxxpdada@outlook.com>
2025-09-23 22:53:01 +0800

    bugfix:修复制作安山糖时会被火山灰阻断的bug

--- kubejs/server_scripts/server_events/recipes/andesite_line.js
+++ kubejs/server_scripts/server_events/recipes/andesite_line.js
@@ -1,12 +1,21 @@
 ServerEvents.recipes(event => {
     global.createRecipes.init(event)
 
-    event.recipes.anvilcraft.block_compress(['minecraft:andesite', 'anvilcraft:sugar_block'], 'foand:andesite_sugar_block')
+    // 方块压缩
+    event.recipes.anvilcraft.block_compress(['anvilcraft:sugar_block', 'minecraft:andesite'], 'foand:andesite_sugar_block')
     event.recipes.anvilcraft.item_inject('minecraft:andesite', 'anvilcraft:sugar_block', 'foand:andesite_sugar_block')
-    event.recipes.create.deploying('foand:andesite_sugar_block', ['anvilcraft:sugar_block', 'minecraft:andesite'])
 
+    // 物品压缩
     event.recipes.anvilcraft.item_compress(
         ['foand:andesite_sugar_block', 'foand:poppy_melon_juice', 'foand:andesite_sugar_block', 'foand:poppy_melon_juice'],
         [ChanceItemStack.of('create:andesite_alloy'), ChanceItemStack.of('2x minecraft:glass_bottle')]
     );
+
+
+
+
+
+    event.recipes.create.deploying('foand:andesite_sugar_block', ['anvilcraft:sugar_block', 'minecraft:andesite'])
+
+
 })
