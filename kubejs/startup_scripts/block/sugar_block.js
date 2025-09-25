002ec6f0b0a13ae43e2e2691be2cc70234babd58
boon <qxxpdada@outlook.com>
2025-09-25 10:21:50 +0800

    remove:删除自定义糖块

--- kubejs/startup_scripts/block/sugar_block.js
+++ kubejs/startup_scripts/block/sugar_block.js
@@ -1,12 +1,12 @@
 StartupEvents.registry('block', event => {
-    // 糖块
-    event.create('anvilcraft:sugar_block')
-        .hardness(1.0)       // 硬度
-        .resistance(1.0)     // 爆炸抗性
-        .soundType('sand')   // 材质
-        .fullBlock(true)     // 完整方块
-        .opaque(true)        // 不透明   
-        .tagBlock('minecraft:mineable/shovel') // 可用铲子挖掘
+    // // 糖块
+    // event.create('anvilcraft:sugar_block')
+    //     .hardness(1.0)       // 硬度
+    //     .resistance(1.0)     // 爆炸抗性
+    //     .soundType('sand')   // 材质
+    //     .fullBlock(true)     // 完整方块
+    //     .opaque(true)        // 不透明   
+    //     .tagBlock('minecraft:mineable/shovel') // 可用铲子挖掘
 
     // 安山糖块
     event.create('foand:andesite_sugar_block')
