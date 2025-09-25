4ad72e99c2d8187658e9424a07c9930bfb8edee1
boon <qxxpdada@outlook.com>
2025-09-25 11:48:10 +0800

    feat:修改蟹笼lootTable，蟹钳产量大增

--- kubejs/server_scripts/loot/crab_trap.js
+++ kubejs/server_scripts/loot/crab_trap.js
@@ -1,1 +1,13 @@
-//蟹笼的战利品表
+LootJS.lootTables(event => {
+
+    event
+        .getLootTable("anvilcraft:gameplay/crab_trap/common")
+        .firstPool()
+        .modifyItemEntry(itemEntry => {
+            if (itemEntry.item.id === "anvilcraft:crab_claw")
+                itemEntry.setWeight(15)
+            if (itemEntry.item.id === "anvilcraft:minecraft:seagrass")
+                itemEntry.setWeight(5)
+        })
+
+})
