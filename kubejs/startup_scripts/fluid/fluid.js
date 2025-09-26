4721f9bf72489b39dfacde598bc5ddf520a28900
L-Bromine <lishaofan_31415926@163.com>
2025-09-26 10:09:52 +0800

    Need Fix：取消贴图的注册

--- kubejs/startup_scripts/fluid/fluid.js
+++ kubejs/startup_scripts/fluid/fluid.js
@@ -7,11 +7,14 @@ StartupEvents.registry('fluid', event => {
 
     // 注册液态灵魂
     event.create('foand:soulsteel', 'thin')
-        .stillTexture('foand:fluid.soulsteel.still')
-        .flowingTexture('foand:fluid.soulsteel.flowing')
-        
+        // .stillTexture('foand:fluid.soulsteel.still')
+        // .flowingTexture('foand:fluid.soulsteel.flowing')
+        .tint(0x6a5244)
+
     // 注册烈焰血
     event.create('foand:blaze', 'thick')
-        .stillTexture('foand:fluid.blaze.still')
-        .flowingTexture('foand:fluid.blaze.flowing')
+        // .stillTexture('foand:fluid.blaze.still')
+        // .flowingTexture('foand:fluid.blaze.flowing')
+        .tint(0xfed134)
+
 })
