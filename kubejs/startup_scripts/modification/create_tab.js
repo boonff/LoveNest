c18b284fbbc366c3167da7c936032bf7fa78d90f
L-Bromine <lishaofan_31415926@163.com>
2025-09-26 11:27:52 +0800

    添加创造模式物品栏

--- kubejs/startup_scripts/modification/create_tab.js
+++ kubejs/startup_scripts/modification/create_tab.js
@@ -1,7 +1,13 @@
-StartupEvents.modifyCreativeTab('kubejs:tab', event => {
+StartupEvents.modifyCreativeTab('foand:foand_tab', event => {
     let items = [
-        'foand:poppy_melon',
-        'foand:poppy_melon_sand'
+        'foand:melt_gem_block',
+        'foand:gem_spring',
+        'foand:lava_spring',
+        'foand:andesite_sugar_block',
+        'foand:andesite_sugar',
+        'foand:bamboo_string',
+        'foand:poppy_melon_slice',
+        'foand:poppy_melon_juice'
     ]
     items.forEach(item => {
         event.add(item)
