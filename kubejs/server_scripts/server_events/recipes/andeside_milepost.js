81d86fa42d4a0ef26792e64ca2dcb1f46e1dcf3a
L-Bromine <lishaofan_31415926@163.com>
2025-10-07 00:16:47 +0800

    添加烈焰血配方，移动文件结构

--- kubejs/server_scripts/server_events/recipes/andeside_milepost.js
+++ kubejs/server_scripts/server_events/recipes/andeside_milepost.js
@@ -9,4 +9,15 @@ ServerEvents.recipes(event => {
 
     event.recipes.create.mixing([Fluid.of('foand:zinc_iodide_solution', 250), 'minecraft:green_dye'], [Fluid.water(250), 'foand:iodine', 'minecraft:cactus'])
     event.recipes.create.mixing(['foand:iodine', 'create:crushed_raw_zinc'], [Fluid.of('foand:zinc_iodide_solution', 250)]).heated()
+
+    // 烈焰人相关配方
+    event.recipes.create.mixing(Fluid.of('foand:blaze', 50), [Fluid.of('foand:soulsteel', 1000), Fluid.lava(100)])
+
+    const blaze_head = 'foand:blaze_head'
+    event.recipes.create.item_application(
+        'create:blaze_burner',
+        ['create:empty_blaze_burner', blaze_head]
+    )
+
+    event.recipes.create.filling(blaze_head, ['#create:packages', Fluid.of('foand:blaze', 1000)])
 })
