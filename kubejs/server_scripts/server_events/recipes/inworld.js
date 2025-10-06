3028b462c9b8381afe66a8bcb98fb858c38254ee
L-Bromine <lishaofan_31415926@163.com>
2025-10-06 19:02:44 +0800

    添加砸黑曜石产生黑曜石粉配方

---
+++ kubejs/server_scripts/server_events/recipes/inworld.js
@@ -0,0 +1,40 @@
+ServerEvents.recipes(event => {
+    // 铁砧砸黑曜石产生黑曜石粉
+    event.custom({
+        "type": "anvillib:in_world_recipe",
+        "compatible": true,
+        "conflicting": [],
+        "non_conflicting": [
+            {
+                "type": "anvillib:has_block",
+                "offset": [
+                    0.0,
+                    -1.0,
+                    0.0
+                ],
+                "predicate": {
+                    "blocks": "minecraft:obsidian",
+                    "properties": []
+                }
+            }
+        ],
+        "icon": {
+            "id": "create:powdered_obsidian",
+            "count": 1
+        },
+        "outcomes": [
+            {
+                "type": "anvillib:spawn_item",
+                "item": "create:powdered_obsidian",
+                "offset": [
+                    0.0,
+                    0.5,
+                    0.0
+                ]
+            }
+        ],
+        "priority": 5,
+        "max_efficiency": 1,
+        "trigger": "anvilcraft:on_anvil_fall_on"
+    })
+})
