33902b7c62d009569b94489956b5fa8e9b8d439e
L-Bromine <lishaofan_31415926@163.com>
2025-10-08 00:18:51 +0800

    添加催化剂碘消耗概率

--- kubejs/server_scripts/server_events/recipes/andeside_milepost.js
+++ kubejs/server_scripts/server_events/recipes/andeside_milepost.js
@@ -8,7 +8,7 @@ ServerEvents.recipes(event => {
     event.recipes.create.mixing('foand:iodine', Fluid.of('foand:iodine_extract_oil', 1000)).heated()
 
     event.recipes.create.mixing([Fluid.of('foand:zinc_iodide_solution', 250), 'minecraft:green_dye'], [Fluid.water(250), 'foand:iodine', 'minecraft:cactus'])
-    event.recipes.create.mixing(['foand:iodine', 'create:crushed_raw_zinc'], [Fluid.of('foand:zinc_iodide_solution', 250)]).heated()
+    event.recipes.create.mixing([{ id: 'foand:iodine', chance: 0.99 }, 'create:crushed_raw_zinc'], [Fluid.of('foand:zinc_iodide_solution', 250)]).heated()
 
     // 烈焰人相关配方
     event.recipes.create.mixing(Fluid.of('foand:blaze', 50), [Fluid.of('foand:soulsteel', 250), Fluid.lava(100)])
