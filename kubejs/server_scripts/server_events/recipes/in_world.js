4c90410f5c04fc9f12c94415c0edcb81a19d1914
boon <qxxpdada@outlook.com>
2026-08-14 08:35:38 +0800

    feat: 更新铁砧相关配方，添加随机掉落物品机制

--- kubejs/server_scripts/server_events/recipes/in_world.js
+++ kubejs/server_scripts/server_events/recipes/in_world.js
@@ -38,7 +38,7 @@ ServerEvents.recipes(event => {
         "trigger": "anvilcraft:on_anvil_fall_on"
     })
 
-    // 金矿 TODO 随机采矿
+    // 铁砧砸朝下的机械钻头 + 下方有尸块 → 随机掉落金粒 TODO 随机采矿
     event.custom({
         "type": "anvillib:in_world_recipe",
         "compatible": true,
@@ -79,17 +79,42 @@ ServerEvents.recipes(event => {
             {
                 "type": "anvillib:choose_one",
                 "choices": [
+                    //设置多个掉落物品，是为了随机掉落方向
+                    {
+                        "outcome": {
+                            "type": "anvillib:spawn_item",
+                            "item": 'minecraft:rotten_flesh',
+                            "count": 1,
+                            "offset": [ 0.5, -2.0, 0.7 ]
+                        },
+                        "weight": 3.0
+                    },
+                    {
+                        "outcome": {
+                            "type": "anvillib:spawn_item",
+                            "item": 'minecraft:rotten_flesh',
+                            "count": 2,
+                            "offset": [ 0.4, -2.0, -0.3 ]
+                        },
+                        "weight": 3.0
+                    },
                     {
                         "outcome": {
                             "type": "anvillib:spawn_item",
                             "item": 'minecraft:gold_nugget',
-                            "offset": [
-                                0.0,
-                                0.5,
-                                0.0
-                            ]
+                            "count": 1,
+                            "offset": [ -0.8, -2.0, 0.5 ]
                         },
-                        "weight": 20.0
+                        "weight": 2.0
+                    },
+                    {
+                        "outcome": {
+                            "type": "anvillib:spawn_item",
+                            "item": 'minecraft:gold_nugget',
+                            "count": 2,
+                            "offset": [ 0.7, -2.0, 0.2 ]
+                        },
+                        "weight": 2.0
                     }
                 ]
             }
